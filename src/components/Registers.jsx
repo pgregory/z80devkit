e3a46552d756e7703fbc63129c7f9e8947c02df9
Paul Gregory <aqsis1@gmail.com>
2017-09-18 23:53:29 +0100

    Minor updates to macros

--- src/components/Registers.jsx
+++ src/components/Registers.jsx
@@ -8,28 +8,32 @@ class Registers extends React.Component {
         <table>
           <tbody>
             <tr>
-              <th>A:</th><td colSpan={2}>{hexByte(this.props.registers.A)}</td>
+              <th>A:</th><td>{hexByte(this.props.registers.A)}</td>
+              <th>A':</th><td>{hexByte(this.props.registers.A_)}</td>
             </tr>
             <tr>
               <th>BC:</th><td>{hexWord(this.props.registers.BC)}</td>
+              <th>BC':</th><td>{hexWord(this.props.registers.BC_)}</td>
             </tr>
             <tr>
               <th>DE:</th><td>{hexWord(this.props.registers.DE)}</td>
+              <th>DE':</th><td>{hexWord(this.props.registers.DE_)}</td>
             </tr>
             <tr>
               <th>HL:</th><td>{hexWord(this.props.registers.HL)}</td>
+              <th>HL':</th><td>{hexWord(this.props.registers.HL_)}</td>
             </tr>
             <tr>
-              <th>IX:</th><td>{hexWord(this.props.registers.IX)}</td>
+              <th>IX:</th><td colSpan={3}>{hexWord(this.props.registers.IX)}</td>
             </tr>
             <tr>
-              <th>IY:</th><td>{hexWord(this.props.registers.IY)}</td>
+              <th>IY:</th><td colSpan={3}>{hexWord(this.props.registers.IY)}</td>
             </tr>
             <tr>
-              <th>PC:</th><td>{hexWord(this.props.registers.PC)}</td>
+              <th>PC:</th><td colSpan={3}>{hexWord(this.props.registers.PC)}</td>
             </tr>
             <tr>
-              <th>SP:</th><td>{hexWord(this.props.registers.SP)}</td>
+              <th>SP:</th><td colSpan={3}>{hexWord(this.props.registers.SP)}</td>
             </tr>
           </tbody>
         </table>
